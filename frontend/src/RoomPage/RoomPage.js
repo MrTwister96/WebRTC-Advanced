1cba5987744f104fdf7a8b26c3af9fb86eafd622
Schalk Olivier <schalkolivier@vegamine.com>
2021-09-22 17:07:00 +0200

    Participant Section

--- frontend/src/RoomPage/RoomPage.js
+++ frontend/src/RoomPage/RoomPage.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import ParticipantsSection from "./ParticipantsSection/ParticipantsSection";
+import VideoSection from "./VideoSection/VideoSection";
+import ChatSection from "./ChatSection/ChatSection";
+import RoomLabel from "./RoomLabel";
 
-const RoomPage = () => {
-    return <div>Hello here is Room Page</div>;
+import "./RoomPage.css";
+
+const RoomPage = ({ roomId }) => {
+    return (
+        <div className="room_container">
+            <ParticipantsSection />
+            <VideoSection />
+            <ChatSection />
+            <RoomLabel roomId={roomId} />
+        </div>
+    );
 };
 
-export default RoomPage;
+const mapStoreStateToProps = (state) => {
+    return {
+        ...state,
+    };
+};
+
+export default connect(mapStoreStateToProps)(RoomPage);
---
+++ frontend/src/RoomPage/VideoSection/VideoSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoSection = () => {
+    return (
+        <div className="video_section_container">
+            Video Section
+        </div>
+    );
+};
+
+export default VideoSection;
